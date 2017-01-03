49361b4a50b867290c379e40aea24d974e0ab7bd
ggli <lixianglin850302@163.com>
2017-01-03 16:37:40 +0800

    Initial Commit

---
+++ ggliLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ggliLive
+//
+//  Created by xianglin li on 2017/1/3.
+//  Copyright © 2017年 xianglin li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ggliLive/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ggliLive
+//
+//  Created by xianglin li on 2017/1/3.
+//  Copyright © 2017年 xianglin li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
